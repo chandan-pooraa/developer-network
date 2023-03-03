7c086108aa3cab7378180eb08d704e02d01c74bf
Avinash Pandey <avinash@busy.in>
2023-03-03 10:46:03 +0530

    pushing db and common files

--- database/lib.go
+++ database/lib.go
@@ -1,3 +1,44 @@
 // Contains all the common methods related to database
-
 package database
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+)
+
+func PanicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+var DB *pg.DB
+
+func Dbconnection() {
+	DB = pg.Connect(&pg.Options{
+		User:     "postgres",
+		Password: "1234",
+		Addr:     "localhost:5432",
+		Database: "postgres",
+	})
+	ctx := context.Background()
+	err := DB.Ping(ctx)
+	if err != nil {
+		log.Fatal("Failed to connect to db")
+	}
+	PanicIf(err)
+}
+
+func CreateT(db *pg.DB, entity []interface{}) {
+
+	for _, model := range entity {
+
+		err := db.Model(model).CreateTable(&orm.CreateTableOptions{
+			IfNotExists: true,
+		})
+		PanicIf(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"developer-network/api/post"
+	"developer-network/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+// func init() {
+// 	database.Dbconnection()
+// }
+
+func main() {
+	router := gin.Default()
+	database.Dbconnection()
+	defer database.DB.Close()
+	model := []interface{}{
+		(*(database.Post))(nil),
+	}
+	database.CreateT(database.DB, model)
+	post.Init(router)
+
+	router.Run()
+}

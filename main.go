f5289c3d19475b10ca73cc3b048b7372f31ec2a3
Avinash Pandey <avinash@busy.in>
2023-03-13 12:40:22 +0530

    m

--- main.go
+++ main.go
@@ -3,31 +3,11 @@ package main
 import (
 	"developer-network/database"
 
-	//"go/doc/comment"
-
-	// "fmt"
-	// "go/doc/comment"
-
 	"github.com/gin-gonic/gin"
 )
 
 // func init() {
 // 	database.Dbconnection()
-// }
-
-// func main() {
-// 	router := gin.Default()
-// 	database.Dbconnection()
-// 	defer database.DB.Close()
-// 	model := []interface{}{
-// 		(*(database.Post))(nil),
-// 		(*(database.User))(nil),
-// 		(*(database.Comment))(nil),
-// 	}
-// 	database.CreateT(database.DB, model)
-// 	comment.Init(router)
-
-// 	router.Run()
 // }
 
 func main() {

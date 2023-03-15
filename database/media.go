f73bf8c3a5c9c964bf0ba559af8bcd658bce39f7
sshreya0 <shreya@busy.in>
2023-03-15 12:29:42 +0530

    done media

--- database/media.go
+++ database/media.go
@@ -1,1 +1,13 @@
 package database
+
+/*
+	name: media -- tableName
+*/
+
+type Media struct {
+	// tableName struct{} `sql:"media"` --> to alter tablename in database
+	ID       uint64 `json:"id" pg:"id,pk"`
+	Name     string `json:"name" pg:"name,notnull"`
+	Url      string `json:"url" pg:"url,unique,notnull"`
+	Category string `json:"category" pg:"category,notnull"`
+}
